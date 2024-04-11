87cb9e2e0198dcad066688003af6857ecc2151e0
DavidAbderhalden <david.abderhalden@hotmail.com>
2024-04-11 14:51:57 +0200

    Fixes bug that /admin/users.php page didn't load the session

--- src/admin/users.php
+++ src/admin/users.php
@@ -1,4 +1,5 @@
 <?php
+include __DIR__.'/../session/session.php';
 include __DIR__.'/../fw/db.php';
 
 if (!isset($_SESSION['username'])) {
--- src/session/session.php
+++ src/session/session.php
@@ -4,6 +4,14 @@ use JetBrains\PhpStorm\NoReturn;
 
 session_start();
 
+/*
+session_start([
+    'cookie_lifetime' => 43200,
+    'cookie_secure' => true,
+    'cookie_httponly' => true
+]);
+*/
+
 #[NoReturn] function terminateSession(): void {
     session_destroy();
     header('location:/index.php');
